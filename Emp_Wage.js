fbd8389a89c8b2c6c697df259c461f69cf58fe22
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-03 23:45:04 +0530

    added code to check employee is present

--- Emp_Wage.js
+++ Emp_Wage.js
@@ -1,1 +1,16 @@
-console.log("welcome emp_wage project")
+
+//UC-1 Check Employee is Present!
+
+console.log("Welcome To EmloyeeWage!!");
+
+
+let Employee_Attandance;
+
+let Check = Math.random()*2;
+
+if (Check != 0){
+    Employee_Attandance = "Present";
+}else
+    Employee_Attandance = "Absent";
+
+console.log(Employee_Attandance);
