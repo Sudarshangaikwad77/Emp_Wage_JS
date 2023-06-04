b80dbf3eef2079206b8707d37d77e29ef34e5036
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-06-04 22:51:06 +0530

    calculate wage ffor different companies

--- Emp_Wage.js
+++ Emp_Wage.js
@@ -1,90 +1,122 @@
-
-console.log("Welcome To EmployeeWage!!");
-
-const wagePerHour = 20;
-const fullDayHours = 8;
-const partTimeHours = 4;
-const workingDaysPerMonth = 20;
-
-let totalWage = 0;
-let totalHoursWorked = 0;
-let dailyWageArray = [];
-let employeeRecords = [];
-
-for (let day = 1; day <= workingDaysPerMonth; day++) {
-    let employeeAttendance;
-    let check = Math.floor(Math.random() * 3);
-
-    switch (check) {
-        case 0:
-            employeeAttendance = "Absent";
-            break;
-        case 1:
-            employeeAttendance = "Part-Time";
-            break;
-        case 2:
-            employeeAttendance = "Full-Time";
-            break;
-        default:
-            employeeAttendance = "Invalid";
+class EmployeeWageCalculator {
+    constructor(company, wagePerHour, numWorkingDays, maxWorkingHours) {
+        this.part_time_hour = 4;
+        this.full_day_hour = 8;
+        this.totalWorkingDays = 0;
+        this.totalWorkingHours = 0;
+        this.monthlyWage = 0;
+        this.company = company;
+        this.wagePerHour = wagePerHour;
+        this.numWorkingDays = numWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+        this.totalWage = 0;
     }
-
-    let employeeWage = 0;
-    let hoursWorked = 0;
-
-    switch (employeeAttendance) {
-        case "Part-Time":
-            employeeWage = wagePerHour * partTimeHours;
-            hoursWorked = partTimeHours;
-            break;
-        case "Full-Time":
-            employeeWage = wagePerHour * fullDayHours;
-            hoursWorked = fullDayHours;
-            break;
+  
+    calculateWage() {
+        console.log(`Calculating wage for company ${this.company}`);
+        let dailyWage;
+        while (this.totalWorkingDays < this.numWorkingDays && this.totalWorkingHours < this.maxWorkingHours) {
+            const newAttendance = Math.floor(Math.random() * 3); // 0 for absent, 1 for part-time, 2 for full-time
+            switch (newAttendance) {
+                case 1:
+                    dailyWage = this.wagePerHour * this.part_time_hour;
+                    this.totalWorkingHours += this.part_time_hour;
+                    break;
+                case 2:
+                    dailyWage = this.wagePerHour * this.full_day_hour;
+                    this.totalWorkingHours += this.full_day_hour;
+                    break;
+                default:
+                    dailyWage = 0;
+                    break;
+            }
+            this.totalWorkingDays++;
+            this.monthlyWage += dailyWage;
+        }
+        this.totalWage += this.monthlyWage;
+        return this.monthlyWage;
     }
-
-    dailyWageArray.push(employeeWage);
-    totalWage += employeeWage;
-    totalHoursWorked += hoursWorked;
-
-    let record = {
-        day: day,
-        hoursWorked: hoursWorked,
-        wageEarned: employeeWage
-    };
-    employeeRecords.push(record);
-}
-
-console.log("Daily Wage Array: " + dailyWageArray);
-console.log("Total Wage for the Month: " + totalWage);
-console.log("Employee Records: ", employeeRecords);
-
-// a. Calc total Wage and total hours worked using arrow functions
-const calcTotal = (accumulator, record) => accumulator + record.wageEarned;
-const totalWageUsingArrow = employeeRecords.reduce(calcTotal, 0);
-const totalHoursWorkedUsingArrow = employeeRecords.reduce((acc, record) => acc + record.hoursWorked, 0);
-
-console.log("Total Wage for the Month (using arrow function): " + totalWageUsingArrow);
-console.log("Total Hours Worked (using arrow function): " + totalHoursWorkedUsingArrow);
-
-// b. Show the full working days using forEach and arrow function
-console.log("Full Working Days (using forEach):");
-employeeRecords.forEach(record => {
-    if (record.hoursWorked === fullDayHours) {
-        console.log("Day " + record.day);
+  
+    getCompany() {
+        return this.company;
     }
-});
-
-// c. Show the part working days using Map and reducing to a string array
-const partWorkingDays = employeeRecords
-    .filter(record => record.hoursWorked === partTimeHours)
-    .map(record => "Day " + record.day);
-
-console.log("Part Working Days (using Map):", partWorkingDays);
-
-// d. Show the non-working days using Map function
-const nonWorkingDays = employeeRecords
-    .filter(record => record.hoursWorked === 0)
-    .map(record => "Day " + record.day);
-
-console.log("Non-working Days (using Map):", nonWorkingDays);
+  
+    getTotalWage() {
+        return this.totalWage;
+    }
+  }
+  
+  class CompanyEmpWage {
+    constructor(company, wagePerHour, numWorkingDays, maxWorkingHours) {
+        this.company = company;
+        this.wagePerHour = wagePerHour;
+        this.numWorkingDays = numWorkingDays;
+        this.maxWorkingHours = maxWorkingHours;
+        this.totalWage = 0;
+    }
+  
+    getTotalWage() {
+        return this.totalWage;
+    }
+  
+    getCompany() {
+        return this.company;
+    }
+  
+    getWagePerHour() {
+        return this.wagePerHour;
+    }
+  
+    getNumWorkingDays() {
+        return this.numWorkingDays;
+    }
+  
+    getMaxWorkingHours() {
+        return this.maxWorkingHours;
+    }
+  
+    setTotalWage(totalWage) {
+        this.totalWage = totalWage;
+    }
+  }
+  
+  class EmpWageBuilder {
+    constructor() {
+        this.companies = [];
+        this.numCompanies = 0;
+    }
+  
+    addCompany(company, wagePerHour, numWorkingDays, maxWorkingHours) {
+        const emp = new CompanyEmpWage(company, wagePerHour, numWorkingDays, maxWorkingHours);
+        this.companies[this.numCompanies++] = emp;
+    }
+  
+    computeWages() {
+        for (let i = 0; i < this.numCompanies; i++) {
+            const wage = new EmployeeWageCalculator(
+                this.companies[i].getCompany(),
+                this.companies[i].getWagePerHour(),
+                this.companies[i].getNumWorkingDays(),
+                this.companies[i].getMaxWorkingHours()
+            ).calculateWage();
+            this.companies[i].setTotalWage(wage);
+            console.log(`Total wage for ${this.companies[i].getCompany()} is ${this.companies[i].getTotalWage()}`);
+        }
+    }
+  }
+  
+  class EmpWage {
+    static main() {
+      const empWageBuilder = new EmpWageBuilder();
+      empWageBuilder.addCompany("Company A", 20, 20, 100);
+      empWageBuilder.addCompany("Company B", 25, 25, 120);
+      empWageBuilder.computeWages();
+  }
+  }
+  
+  EmpWage.main();
+  
+    
+    
+  
+  
